a0bd035c926e873dc432003a6118a6eb2bb93b07
Zenos <mdzenos@gmail.com>
2023-12-09 06:15:11 +0000

    Maintain

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>F4U.FUN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- main css -->
+    <link rel="stylesheet" href="public/css/503.css">
+</head>
+
+<body>
+    <!-- content section -->
+    <section class="content">
+        <img src="public/img/illustration.svg" alt="img">
+        <div class="text">
+            <h1>Trang web đang bảo trì</h1>
+            <p class="w-25">Chúng tôi xin lỗi vì sự bất tiện này, vui vòng quay lại sau !</p>
+        </div>
+    </section>
+    <!-- footer section -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
